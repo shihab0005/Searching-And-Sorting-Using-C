70724f28e2b03aa8db45608c125b2318d47cd6b3
SHIHAB KHAN <41753677+shihab0005@users.noreply.github.com>
2020-02-12 10:38:46 -0800

    Add files via upload

---
+++ BubbleSort.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+	
+	int i,j,n,temp;
+	printf("Enter Range :");
+	scanf("%d",&n);
+	int arr[n];
+	printf("Enter %d value :\n",n);
+	for(i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-1-i;j++){
+			if(arr[j]>arr[j+1]){
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+	printf("After Bubble Sort :");
+	for(i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+}
